ac80a2a06e08069f8ae700a19c678a75c430d32a
Mattias Lövkvist <mattiaslovkvist@gmail.com>
2020-11-02 22:58:34 +0100

    Fixed window.postMessage communication

--- resources/views/livewire/skills.blade.php
+++ resources/views/livewire/skills.blade.php
@@ -1,18 +1,12 @@
 <div>
-    <iframe id="skill-calculator" class="h-screen w-screen" src="https://tools.torchlightfansite.com/tlfskillcalculator/build-dm.html" />
+    <iframe id="skill-calculator" class="h-screen w-screen" src="https://tools.torchlightfansite.com/tlfskillcalculator/build-dm.html"></iframe>
 </div>
 
 <script>
-    let skillCalculator = document.querySelector('#skill-calculator');
-
-    skillCalculator.addEventListener('load', () => {
-        console.log('hello world');
-    });
-
-    skillCalculator.addEventListener('message', (event) => {
-        if (event.origin !== "https://tools.torchlightfansite.com/")
+    window.addEventListener('message', (event) => {
+        if (event.origin !== "https://tools.torchlightfansite.com")
         return;
 
-        console.log('received message ' + event.data);
+        console.log(event.data);
     }, false);
 </script>
