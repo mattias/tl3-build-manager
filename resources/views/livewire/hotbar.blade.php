54b339550f747f403823ba7e817bc3aed8079cfc
Mattias Lövkvist <mattiaslovkvist@gmail.com>
2020-11-17 23:14:41 +0100

    Styled hotbar a little

--- resources/views/livewire/hotbar.blade.php
+++ resources/views/livewire/hotbar.blade.php
@@ -1,29 +1,29 @@
 <div class="flex">
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[0] }}" />
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[0] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[1] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[2] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[3] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[4] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[5] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[6] }}" />
+    </div>
+    <div class="w-1/9 w-12 h-12 mr-1">
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[7] }}" />
     </div>
     <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[1] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[2] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[3] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[4] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[5] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[6] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[7] }}" />
-    </div>
-    <div class="w-1/9 w-12 h-12">
-        <img src="{{ $hotbar[8] }}" />
+        <img class="rounded shadow cursor-pointer" src="{{ $hotbar[8] }}" />
     </div>
 </div>
